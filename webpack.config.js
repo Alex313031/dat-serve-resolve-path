f5adc2505767c4e4e3cb2f2573c98fd5658c1df7
Alexander Frick <Alex313031@gmail.com>
2023-05-20 15:57:18 -0500

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,8 @@ module.exports = {
   entry: './index.js',
   mode: 'production',
   output: {
-    libraryTarget: 'window',
-    library: 'datServeResolvePath',
+    // libraryTarget: 'win',
+    // library: 'datServeResolvePath',
     path: __dirname,
     filename: 'dist.js'
   }
